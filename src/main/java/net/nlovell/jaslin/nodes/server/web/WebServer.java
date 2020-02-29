3c94f70e394590f87606efa0c407907e5e515b18
nlovell-net <43500865+nlovell-net@users.noreply.github.com>
2020-02-29 20:02:36 +0000

    basic endpoint work

--- src/main/java/net/nlovell/jaslin/nodes/server/web/WebServer.java
+++ src/main/java/net/nlovell/jaslin/nodes/server/web/WebServer.java
@@ -1,7 +1,9 @@
 package net.nlovell.jaslin.nodes.server.web;
 
 import net.nlovell.jaslin.nodes.server.web.handlers.HelloWorld;
+import net.nlovell.jaslin.nodes.server.web.handlers.playpause;
 import net.nlovell.jaslin.tools.ConfigReader;
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Server;
 
 /**
@@ -10,6 +12,8 @@ import org.eclipse.jetty.server.Server;
 public class WebServer {
 
     private Server server;
+    Logger log;
+
 
     public WebServer() {
         try {
@@ -18,14 +22,22 @@ public class WebServer {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        log = Logger.getLogger(WebServer.class);
     }
 
     private Server getServer(){
         if(server==null){
             server = new Server(Integer.parseInt(ConfigReader.getConfigReader().getConfigOrDefault("PORT")));
             server.setHandler(new HelloWorld());
+            server.setHandler(new playpause());
+
         }
         return this.server;
+    }
+
+    private void endpoint_log(String ep){
+        log.info("Endpoint accessed: " + ep);
     }
 
 }
